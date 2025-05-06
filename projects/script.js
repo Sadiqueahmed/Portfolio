8c8113e64a10b1bdfb3112500439f6ad30c801e0
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-06 19:46:04 +0530

    /

--- projects/script.js
+++ projects/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     $('#menu').click(function () {
@@ -20,8 +21,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Sadique Ahmed";
-            $("#favicon").attr("href", "/assets/images/Emoji-avatar.png");
+            document.title = "Projects | Portfolio Jigar Sable";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
@@ -105,17 +106,19 @@ getProjects().then(data => {
 })
 // fetch projects end
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+// <!--Start of Tawk.to Script-->
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6819f246138f991917bc8705/1iqilkp99';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
 })();
-// End of Tawk.to Live Chat
+
+// <!--End of Tawk.to Script-->
 
 // disable developer mode
 document.onkeydown = function (e) {
