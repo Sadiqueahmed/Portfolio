4ca2a1c662ac7fe9b884b02f699ff3070211f15e
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-06 11:41:47 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,8 +20,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            document.title = "Projects | Portfolio Sadique Ahmed";
+            $("#favicon").attr("href", "/assets/images/Emoji-avatar.png");
         }
         else {
             document.title = "Come Back To Portfolio";
